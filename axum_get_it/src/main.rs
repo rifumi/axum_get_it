d02b40f4458dcd1c397516492e0e70decc4729f3
tadanonorif <89812517+rifumi@users.noreply.github.com>
2023-04-12 00:00:16 +0900

    add logging

--- axum_get_it/src/main.rs
+++ axum_get_it/src/main.rs
@@ -1,11 +1,18 @@
 use axum::{routing::get, Router};
 use std::net::{SocketAddr, Ipv4Addr};
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    // logging
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
     // container port:3000にリクエストを受信した時root()を呼び出す
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3000));
+    tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
